950ad771310c09fe4d9f1f10c92f9c16774af942
GAbriel Falcão <gabrielfdamaceno@gmail.com>
2020-02-12 07:01:55 -0300

    Update impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,30 +13,34 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== TESTE 1: seller findById ===");
-		Seller seller = sellerDao.findById(8);
-		
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== TESTE 2: seller findByDepartment ===");
-		Department deparment  = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(deparment);
-		for(Seller obj : list) {
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("\n=== TESTE 3: seller findByAll ===");		
-		 list = sellerDao.findAll();
-		for(Seller obj : list) {
+
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller update =====");
+		Seller selleri = new Seller();
+		selleri = sellerDao.findById(1);
+		selleri.setName("Martha Waine");
+		sellerDao.update(selleri);
+		System.out.println("Update completed");
 		
-		System.out.println("\n=== TESTE e: seller Insert ===");		
-		Seller sellerNew = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.00,deparment);
-		sellerDao.insert(sellerNew);
-		
-		System.out.println("Insert teste: " + sellerNew.getId());
-		
-	
 	}
 }
